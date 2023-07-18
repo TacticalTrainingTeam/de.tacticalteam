825c045aa97d65dda4340b5b0614735ed8b36168
Isaac <31494412+TFWIsaac@users.noreply.github.com>
2023-07-18 21:36:41 +0200

    Sicherheitsfeature Active Flag in DB

--- resources/views/intern/start.blade.php
+++ resources/views/intern/start.blade.php
@@ -5,21 +5,25 @@
 
     <!-- About -->
     <section class="container g-pt-100 g-pb-70">
-        <h1>Willkommen im TTT Internen Bereich</h1>
-        <p class="lead">Angemeldet als: {{\Illuminate\Support\Facades\Auth::user()->global_name}}<br></p>
         @php
-        $userRolesReadable = \App\Models\User::getAllRolesOfUser(null, true);
-        //$userRoles         = \App\Models\User::getAllRolesOfUser();
+            $userRolesReadable = \App\Models\User::getAllRolesOfUser(null, true);
+            //$userRoles         = \App\Models\User::getAllRolesOfUser();
         @endphp
-        <p class="lead">Du hast folgende Gruppen: {{implode(", ", $userRolesReadable)}}</p>
+        <h1>Willkommen im TTT Internen Bereich</h1>
+        <p class="lead">Angemeldet als: {{\Illuminate\Support\Facades\Auth::user()->global_name}}. Du hast folgende Gruppen: {{implode(", ", $userRolesReadable)}}</p>
         <div class="mt-3">
             @if(\App\Models\User::UserIn(\App\Enums\Roles::Offizier))
-                <x-button-link link="{{route('offizier.missionsteilnahme')}}" title="Missionsteilnahme"/>
-                <x-button-link link="{{route('offizier.user')}}" title="User"/>
+                <p class="lead mt-3 mb-3">Da du zu den Offizieren des TTT gehörst, stehen dir folgende Möglichkeiten zur Verfügung </p>
+                <x-button-link link="{{route('offizier.missionsteilnahme')}}" title="Missionsteilnahme einsehen"/>
+                <x-button-link link="{{route('offizier.user')}}" title="Übersicht alle Mitglieder"/>
             @endif
+            <br>
             @if(\App\Models\User::UserIn(\App\Enums\Roles::Missionsbauer))
-                    <x-button-link link="{{route('missionupload.index')}}" title="Missionsupload"/>
+                <p class="lead mt-3 mb-3">Du bist Missionsbauer bist, kannst du auf den Missionsupload zugreifen</p>
+                <x-button-link link="{{route('missionupload.index')}}" title="Missionsupload"/>
             @endif
+                <br>
+                <p class="lead mt-3 mb-3">Folgende Möglichkeiten stehen dir zur Verfügung</p>
                 <x-button-link link="{{route('squadxml.index')}}" title="SquadXML"/>
         </div>
 
--- routes/auth.php
+++ routes/auth.php
@@ -2,17 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('intern')->group(function () {
-    Route::get('/start', [\App\Http\Controllers\InternController::class, 'index'])->name('start')->middleware('auth');
-    Route::get('/missionsupload', [\App\Http\Controllers\MissionsuploadController::class, 'index'])->name('missionupload.index')->middleware(['auth', \App\Http\Middleware\IsMissionsbauer::class]);
+Route::prefix('intern')->middleware(['auth', \App\Http\Middleware\IsActive::class])->group(function () {
+    Route::get('/start', [\App\Http\Controllers\InternController::class, 'index'])->name('start');
+    Route::get('/missionsupload', [\App\Http\Controllers\MissionsuploadController::class, 'index'])->name('missionupload.index')->middleware([\App\Http\Middleware\IsMissionsbauer::class]);
     Route::get('/missionsupload/upload', function () {
         return view('intern.missionupload.upload');
-    })->name('missionsupload.upload')->middleware(['auth', \App\Http\Middleware\IsMissionsbauer::class]);
-    Route::post('/missionsupload/store', [\App\Http\Controllers\MissionsuploadController::class, 'store'])->name('missionsupload.store')->middleware(['auth', \App\Http\Middleware\IsMissionsbauer::class]);
-    Route::get('/offizier/missionsteilnahme', [\App\Http\Controllers\Controller::class, 'missionsteilnahme'])->name('offizier.missionsteilnahme')->middleware(['auth', \App\Http\Middleware\IsOffizier::class]);
-    Route::get('/offizier/user', [\App\Http\Controllers\Controller::class, 'uebersicht'])->name('offizier.user')->middleware(['auth', \App\Http\Middleware\IsOffizier::class]);
-    Route::get('/squadxml', [\App\Http\Controllers\SquadXmlController::class, 'index'])->name('squadxml.index')->middleware(['auth']);
-    Route::post('/squadxml/store', [\App\Http\Controllers\SquadXmlController::class, 'store'])->name('squadxml.store')->middleware(['auth']);
-    Route::get('/squadxml/test', [\App\Http\Controllers\SquadXmlController::class, 'test'])->name('squadxml.test')->middleware(['auth']);
-    Route::get('/squadxml/steam', [\App\Http\Controllers\SquadXmlController::class, 'steam'])->name('squadxml.steam')->middleware(['auth']);
+    })->name('missionsupload.upload')->middleware([\App\Http\Middleware\IsMissionsbauer::class]);
+    Route::post('/missionsupload/store', [\App\Http\Controllers\MissionsuploadController::class, 'store'])->name('missionsupload.store')->middleware([\App\Http\Middleware\IsMissionsbauer::class]);
+    Route::get('/offizier/missionsteilnahme', [\App\Http\Controllers\Controller::class, 'missionsteilnahme'])->name('offizier.missionsteilnahme')->middleware([\App\Http\Middleware\IsOffizier::class]);
+    Route::get('/offizier/user', [\App\Http\Controllers\Controller::class, 'uebersicht'])->name('offizier.user')->middleware([\App\Http\Middleware\IsOffizier::class]);
+    Route::get('/squadxml', [\App\Http\Controllers\SquadXmlController::class, 'index'])->name('squadxml.index');
+    Route::post('/squadxml/store', [\App\Http\Controllers\SquadXmlController::class, 'store'])->name('squadxml.store');
+    Route::get('/squadxml/test', [\App\Http\Controllers\SquadXmlController::class, 'test'])->name('squadxml.test');
+    Route::get('/squadxml/steam', [\App\Http\Controllers\SquadXmlController::class, 'steam'])->name('squadxml.steam');
 });
