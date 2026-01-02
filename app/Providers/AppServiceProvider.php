d99d046d9ed941075223b86676c37932a4fc74d4
TheConen <tc@theconen.de>
2026-01-02 18:29:13 +0100

    fix: Since this is served behind traefik that takes care of HTTPS, allow trusted proxies so assets are loaded correctly.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
         //User::observe([UserObserver::class]);
         //DiscordAccessToken::observe([DiscordTokenObserver::class]);
     }
