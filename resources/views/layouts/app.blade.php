5ef6689fdbc64875655923ecf50020af794f35b6
TheConen <tc@theconen.de>
2026-01-02 19:45:06 +0100

    feat: Set default sorting to be by modification date descending

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -451,7 +451,8 @@
         $('#example').DataTable({
             "language": {
                 "url": "//cdn.datatables.net/plug-ins/1.12.1/i18n/de-DE.json"
-            }
+            },
+            "order": [[2, 'desc']]
         });
         $('#missionfinal').on('change', function () {
             value = this.value;
